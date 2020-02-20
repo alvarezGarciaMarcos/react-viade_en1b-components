db47a25e9b42306dd059a4920b1c931eb7437469
Marcos Álvarez García <marcosalvarez1997@gmail.com>
2020-02-20 20:29:03 +0100

    Main Structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from './components/Button/Button'
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import MyNavBar from "./components/Layout/NavBar/NavBar";
+import MyMap from './components/Dashboard/MyMap/MyMap'
+import MyProfile from "./components/user/profile/MyProfile";
+
 
 class App extends Component {
+  state = {
+    userLoggedIn: false
+  }
   render() {
+    const navBar = this.state.userLoggedIn 
+        ? <MyNavBar brandName="Viade_en1b"></MyNavBar>
+        : null 
+    
     return (
-      <div className="App">
-        <Button></Button>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          {navBar}
+          <Switch>
+            <Route exact path="/"></Route>
+            <Route exact path="/profile" component={MyProfile}></Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Layout/NavBar/NavBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+import { BsPerson } from "react-icons/bs";
+
+import {
+  Navbar,
+  NavDropdown,
+  FormControl,
+  Button,
+  Nav,
+  Form
+} from "react-bootstrap";
+
+const MyNavBar = props => {
+  const [navState, setNavState] = useState({
+    loggedIn: false
+  });
+  return (
+    <Navbar className="bg-light" fixed="top" bg="light" expand="lg">
+      <Navbar.Brand href="dashboard">{props.brandName}</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+        <Nav className="justify-content-end" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/routes">My routes</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="routes/upload">Upload route</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="logout" >Log out</Nav.Link>
+          </Nav.Item>
+          
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default MyNavBar;
