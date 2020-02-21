9fe24ced3567cb6e0820c48cfafad0c6ef581d70
Marcos Álvarez García <marcosalvarez1997@gmail.com>
2020-02-21 11:11:49 +0100

    Fixed warnings

--- src/components/Layout/login/Login.js
+++ src/components/Layout/login/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button, Jumbotron } from "react-bootstrap";
+import { Form, Button} from "react-bootstrap";
 import "./Login.css";
 
 export default function Login(props) {
