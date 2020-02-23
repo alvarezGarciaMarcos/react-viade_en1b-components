a602b32c5f8eecaac750de615e2db13c4f73cb28
César García Cabeza <cesareogarcia@hotmail.es>
2020-02-23 19:29:02 +0100

    StoreCreated

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
 
